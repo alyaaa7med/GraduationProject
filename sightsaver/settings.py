6d18be336e0edf4f0829b97b54b3f53bbdad47d4
Alyaa Ahmed <alyaaahmed0643@gmail.com>
2024-03-03 23:36:31 +0200

    change database to sqlite

--- sightsaver/settings.py
+++ sightsaver/settings.py
@@ -83,17 +83,11 @@ WSGI_APPLICATION = 'sightsaver.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-     'default' :{
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'HOST':os.getenv('DB_HOST'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'PORT':os.getenv('DB_PORT'),
-     }
-    
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 
 
 
