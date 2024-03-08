1b3179b9a0d711d0d1f43d75da99055e14c6718a
Alyaa Ahmed <alyaaahmed0643@gmail.com>
2024-03-08 10:15:19 +0200

    check email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,18 +1,18 @@
 
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import DoctorView , PatientView , SendOTP 
+from .views import DoctorView , PatientView , SendOTP
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'Doctor', DoctorView)
 router.register(r'Patient',PatientView)
+router.register(r'SendOTP',SendOTP)
 
 
 urlpatterns = [
    
     path('', include(router.urls)),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # return access + refresh
+    path('Login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # return access + refresh
     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # return new access
-    path('sendOTP',SendOTP)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets  , mixins
 from .models import Doctor , Patient ,User ,otpcode 
 from .serializers import DoctorSerializer , PatientSerializer , EmailSerializerr 
 from rest_framework.parsers import MultiPartParser , FormParser
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , renderer_classes
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 import secrets
 from django.core.mail import send_mail
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from rest_framework_swagger import renderers
+from rest_framework import generics
 
 
 class DoctorView(viewsets.ModelViewSet):
@@ -24,41 +27,42 @@ class PatientView(viewsets.ModelViewSet):
     
  
 
+# @swagger_auto_schema(
+#     method='post',
+#     request_body=EmailSerializerr,
+#     responses={
+#         200: openapi.Response('OTP sent successfully'),
+#         400: openapi.Response('Bad request, validation errors'),
+#         404: openapi.Response('User not found'),
+#         405: openapi.Response('Method not allowed')
+#     },
+#     operation_description="Send OTP code to the user's email address"
+# )
 
-@api_view(['POST'])
-def SendOTP(request):
-    if request.method == 'POST':
-        serializer = EmailSerializerr(data=request.data)
-        if serializer.is_valid():
-            email = serializer.validated_data['email']
-            user = get_object_or_404(User, email=email)
-            otp = otpcode.objects.create(user=user, otp=secrets.token_hex(3)[:5]) #  Fields that have default values or are nullable (blank=True, null=True) can be omitted if you're satisfied with the defaults or if they can be left empty.
-            otp.otp_expires_at = timezone.now() + timezone.timedelta(minutes=5)  # Set OTP expiration time
-            otp.save()
-            
-            subject="Email Code"
-            message = f"""
-                                here is your OTP {otp.otp} 
-                                it expires in 5 minuteuse 
-                                
-                                """
-            sender = "alyaa_ahmed@gmail.com"
-            receiver = [user.email, ]
-       
-        
-        
-            # send email
-            send_mail(
+class SendOTP(viewsets.ModelViewSet):
+    queryset = otpcode.objects.all()
+    serializer_class = EmailSerializerr
+    
+    def create(self, request, *args, **kwargs):
+        user = get_object_or_404(User, email=request.data['email'])
+        user = User.objects.get(email=request.data['email'])
+        otp = otpcode.objects.create(user=user, otp=secrets.token_hex(3)[:5])
+        otp.otp_expires_at = timezone.now() + timezone.timedelta(minutes=5)
+        otp.save()
+
+        subject = "Email Code"
+        message = f"Here is your OTP: {otp.otp}. It expires in 5 minutes."
+        sender = "alyaa_ahmed@gmail.com"
+        receiver = [user.email]
+
+        # Send email
+        send_mail(
                 subject,
                 message,
                 sender,
                 receiver,
                 fail_silently=False,
             )
-  
-            return Response({'message': 'OTP sent successfully'}, status=200)
-    
-        else:
-            errors = serializer.errors
-            return Response(errors, status=400)
-    return Response({'message': 'Not allowed http method'}, status=405)
+
+        return Response({'message': 'OTP sent successfully'}, status=200)
+
